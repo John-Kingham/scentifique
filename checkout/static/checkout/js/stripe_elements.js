3823abd9b56b40ea8ea28aff90e922107469c632
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-03-19 14:17:44 +0000

    feat: add payment submission handling

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,14 +1,17 @@
 // Create and mount the Stripe card payment element
-let stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
-let client_secret = $("#id_client_secret").text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+let clientSecret = $("#id_client_secret").text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
+
 // Leave style as the Stripe default so it looks familiar to users
 let style = {
   base: {
     fontSmoothing: "antialiased",
   },
 };
+
+// Mount the card
 let card = elements.create("card", { style: style });
 card.mount("#card-element");
 
@@ -16,14 +19,52 @@ card.mount("#card-element");
 card.addEventListener("change", function (event) {
   let cardErrorDiv = document.getElementById("card-errors");
   if (event.error) {
-    let html = `
-            <span class="icon mr-1" role="alert">
-                <i class="fas fa-circle-exclamation"></i>
-            </span>
-            <span>${event.error.message}</span>
-        `;
-    $(cardErrorDiv).html(html);
+    $(cardErrorDiv).html(errorMessageHtml(event.error.message));
   } else {
     cardErrorDiv.textContent = "";
   }
 });
+
+// Handle form submission
+let form = document.getElementById("payment-form");
+form.addEventListener("submit", function (event) {
+
+  // Disable inputs
+  event.preventDefault();
+  card.update({ disabled: true });
+  $("#submit-button").attr("disabled", true);
+
+  // Attept card confirmation
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+      },
+    })
+    .then(function (result) {
+      if (result.error) {
+
+        // Display error message
+        let cardErrorsDiv = document.getElementById("card-errors");
+        $(cardErrorsDiv).html(errorMessageHtml(result.error.message));
+
+        // Re-enable inputs so user can fix error
+        card.update({ disabled: false });
+        $("#submit-button").attr("disabled", false);
+
+      } else {
+        if (result.paymentIntent.status === "succeeded") {
+          form.submit();
+        }
+      }
+    });
+});
+
+// Return html to display an error message
+function errorMessageHtml(message) {
+  return `
+    <span class="icon mr-1" role="alert">
+    <i class="fas fa-circle-exclamation"></i>
+    </span>
+    <span>${message}</span>`;
+}
