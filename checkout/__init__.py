928c74efcdb50acefafccafe73b636e4dc9caf3d
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-03-19 16:34:48 +0000

    feat: add checkout success page

---
+++ checkout/__init__.py
@@ -0,0 +1,3 @@
+# Set our bespoke config app to be the default so it runs and attaches our
+# signal receivers.
+default_app_config = "checkout.apps.CheckoutConfig"
--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,27 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 import stripe
 
 from cart.contexts import cart_contents
 from checkout.forms import OrderForm
+from checkout.models import Order, OrderLineItem
+from products.models import Colour, Fragrance, Product
 from scentifique import settings
 
 
 def checkout(request):
     """A view for the checkout page."""
+
+    if request.method == "POST":
+        return _save_order(request)
+    else:
+        return _view_checkout(request)
+
+
+def _view_checkout(request):
+    """Return the checkout page for a get request."""
 
     # Set up Stripe payment intent
     grand_total_pence = round(cart_contents(request)["grand_total"] * 100)
@@ -31,4 +42,70 @@ def checkout(request):
         "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
         "client_secret": payment_intent.client_secret,
     }
+    return render(request, template, context)
+
+
+def _save_order(request):
+    """Save an order to the database."""
+
+    cart = request.session.get("cart", {})
+    form_data = {
+        "full_name": request.POST["full_name"],
+        "email": request.POST["email"],
+        "phone_number": request.POST["phone_number"],
+        "country": request.POST["country"],
+        "postcode": request.POST["postcode"],
+        "town_or_city": request.POST["town_or_city"],
+        "street_address1": request.POST["street_address1"],
+        "street_address2": request.POST["street_address2"],
+        "county": request.POST["county"],
+    }
+    order_form = OrderForm(form_data)
+    if order_form.is_valid():
+        order = order_form.save()
+        for lineitem_key, quantity in cart.items():
+            [product_id, colour_id, fragrance_id] = lineitem_key.split("_")
+            try:
+                product = Product.objects.get(id=product_id)
+                colour = Colour.objects.get(id=colour_id)
+                fragrance = Fragrance.objects.get(id=fragrance_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    colour=colour,
+                    fragrance=fragrance,
+                    quantity=quantity,
+                )
+                order_line_item.save()
+            except (
+                Product.DoesNotExist,
+                Colour.DoesNotExist,
+                Fragrance.DoesNotExist,
+            ):
+                messages.error(
+                    request,
+                    (
+                        "A product, colour or fragrance in your "
+                        "shopping cart is no longer available. "
+                        "Please contact us for assistance."
+                    ),
+                )
+                order.delete()
+                return redirect(reverse("view_cart"))
+        request.session["save_info"] = "save-info" in request.POST
+        return redirect(reverse("checkout_success", args=[order.order_number]))
+    else:
+        messages.error(
+            request, "Error processing form: Please check your information."
+        )
+        return redirect(reverse("view_cart"))
+
+
+def checkout_success(request, order_number):
+    """Handle a successful checkout."""
+
+    request.session.pop("cart")
+    template = "checkout/checkout_success.html"
+    order = get_object_or_404(Order, order_number=order_number)
+    context = {"order": order}
     return render(request, template, context)
