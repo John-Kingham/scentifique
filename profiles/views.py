c17d3ed9a972fd19acff68b26b7ca19b12514717
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-03-23 16:45:17 +0000

    feat: style AllAuth pages

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from profiles.models import UserProfile
 
 
 def profile(request):
     """Display the user profile page."""
 
     template = "profiles/profile.html"
-    context = {}
+    profile = get_object_or_404(UserProfile, user=request.user)
+    context = {
+        "profile": profile,
+    }
     return render(request, template, context)
