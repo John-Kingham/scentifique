959b07af83ac07fcd4a72892bd53a5ca26b758cb
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-03-28 11:57:04 +0000

    fix: fix message when updated cart items match

--- cart/views.py
+++ cart/views.py
@@ -48,19 +48,28 @@ def update_cart(request, cart_item_key):
     cart = request.session.get("cart", {})
     new_cart_item_key = f"{product_id}_{colour_id}_{fragrance_id}"
 
-    if new_cart_item_key == cart_item_key:
+    colour_and_fragrance_unchanged = new_cart_item_key == cart_item_key
+    updated_item_matches_another_item = new_cart_item_key in cart
+    success_message = "Shopping cart updated."
+    if colour_and_fragrance_unchanged:
+        # Just update the quantity
         cart[cart_item_key] = quantity
-    elif new_cart_item_key in cart:
+    elif updated_item_matches_another_item:
+        # Combine the items into a single cart item
         cart[new_cart_item_key] += quantity
         if cart[new_cart_item_key] > settings.MAX_LINE_ITEM_QUANTITY:
             cart[new_cart_item_key] = settings.MAX_LINE_ITEM_QUANTITY
         cart.pop(cart_item_key)
+        success_message += (
+            " The updated item matched another item in the"
+            " cart, so the two have been merged into one."
+        )
     else:
         cart[new_cart_item_key] = quantity
         cart.pop(cart_item_key)
 
     request.session["cart"] = cart
-    messages.success(request, "Shopping cart updated.")
+    messages.success(request, success_message)
     return redirect(reverse("view_cart"))
 
 
