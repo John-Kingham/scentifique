2a58d5fc689ee51fbd3dbf27a01956a67403b701
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2026-07-03 21:08:00 +0100

    test: add profiles tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from .models import UserProfile
+
+
+class UserProfileTests(TestCase):
+
+    def test_user_profile_creation(self):
+        """Test that the user profile is created at user creation"""
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpass123",
+            email="test@email.com",
+        )
+        user.save()
+        self.assertNotEqual(user.userprofile, None)
+        self.assertEqual(UserProfile.objects.count(), 1)
